142361649ac7da9febe003e5d55dbe31ad812d9e
richardgli <ric.li6374@gmail.com>
2024-10-20 00:28:20 -0400

    Update tutorialcode_initial.py

--- tutorialcode/tutorialcode_initial.py
+++ tutorialcode/tutorialcode_initial.py
@@ -11,14 +11,14 @@ def drawMountainSlime(y):
 def drawText(char, x, y, R, G, B):
     print("drawchar " + str(char) + " " + str(x) + " " + str(y) + " " + str(R) + " " + str(G) + " " + str(B))
 
-text = ["Attack"]
+text = "Attack"
 
 for i in range(50):
     print("#frame")
     if i != 0:
         print("clear")
     if math.floor(i / 5) < len(text[0]):
-        drawText(text[0][math.floor(i / 5)], 100 + math.floor(i / 5) * 5, 100, 255, 255, 255)
+        drawText(text[math.floor(i / 5)], 100 + math.floor(i / 5) * 5, 100, 255, 255, 255)
     drawHealthBar(270, 0, (i + 2) * 3, -120, 255, 0, 0)
     drawPlayer((i + 2) * 3)
     drawHealthBar(250, 0, -(i + 2) * 3, 390, 0, 255, 0)
