e2559056df005a65bc0e7a5de16e847275132d50
richardgli <ric.li6374@gmail.com>
2024-10-20 11:15:00 -0400

    Add files via upload

---
+++ tutorialcode/tutorialcode_lobby.py
@@ -0,0 +1,231 @@
+import math
+
+def drawPlayer(y):
+    #print("#frame")
+    print("drawentity player1 320", str(y - 200))
+
+def drawCrystalbat(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawCrystalgolem(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawCrystalknight(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawCrystalscorpion(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawCrystalslime(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawBronze(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawSilver(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawApple(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawWatermelon(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawCrystal_Armor(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawBasic_Crossbow(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawCrystal_Raygun(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawPortal(y):
+    #print("#frame")
+    print("drawentity player1 320", str(y - 100))
+
+def drawText(char, x, y, R, G, B):
+    print("drawchar " + str(char) + " " + str(x) + " " + str(y) + " " + str(R) + " " + str(G) + " " + str(B))
+
+text1 = "Crystal"
+text2 = "Biome"
+text3 = "Rating:"
+text4 = "1/5"
+
+for i in range(60):
+    print("#frame")
+    if i != 0:
+        print("clear 45 60 92")
+    drawText(text1[0], 305, 200, 255, 255, 255)
+    drawText(text1[1], 310, 200, 255, 255, 255)
+    drawText(text1[2], 315, 200, 255, 255, 255)
+    drawText(text1[3], 320, 200, 255, 255, 255)
+    drawText(text1[4], 325, 200, 255, 255, 255)
+    drawText(text1[5], 330, 200, 255, 255, 255)
+    drawText(text1[6], 335, 200, 255, 255, 255)
+
+for i in range(60):
+    print("#frame")
+    if i != 0:
+        print("clear")
+    drawText(text2[0], 310, 200, 255, 255, 255)
+    drawText(text2[1], 315, 200, 255, 255, 255)
+    drawText(text2[2], 320, 200, 255, 255, 255)
+    drawText(text2[3], 325, 200, 255, 255, 255)
+    drawText(text2[4], 330, 200, 255, 255, 255)
+
+
+for i in range(60):
+    print("#frame")
+    if i != 0:
+        print("clear")
+    drawText(text3[0], 305, 200, 255, 255, 255)
+    drawText(text3[1], 310, 200, 255, 255, 255)
+    drawText(text3[2], 315, 200, 255, 255, 255)
+    drawText(text3[3], 320, 200, 255, 255, 255)
+    drawText(text3[4], 325, 200, 255, 255, 255)
+    drawText(text3[5], 330, 200, 255, 255, 255)
+    drawText(text3[6], 335, 200, 255, 255, 255)
+
+
+for i in range(60):
+    print("#frame")
+    if i != 0:
+        print("clear")
+    drawText(text4[0], 315, 200, 255, 255, 255)
+    drawText(text4[1], 320, 200, 255, 255, 255)
+    drawText(text4[2], 325, 200, 255, 255, 255)
+
+
+for i in range(50):
+    print("#frame")
+    if i != 0:
+        print("clear")
+    drawPlayer(0)
+
+def drawPlayer(y):
+    #print("#frame")
+    print("drawentity player1 320", str(y - 200))
+
+def drawCrystalbat(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawCrystalgolem(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawCrystalknight(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawCrystalscorpion(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawCrystalslime(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawBronze(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawSilver(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawApple(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawWatermelon(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawCrystal_Armor(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawBasic_Crossbow(y):
+    #print("#frame")
+    print("drawentity player1 427", str(y - 100))
+
+def drawCrystal_Raygun(y):
+    #print("#frame")
+    print("drawentity player1 213", str(y - 100))
+
+def drawPortal(y):
+    #print("#frame")
+    print("drawentity player1 320", str(y - 100))
+
+def drawText(char, x, y, R, G, B):
+    print("drawchar " + str(char) + " " + str(x) + " " + str(y) + " " + str(R) + " " + str(G) + " " + str(B))
+
+text1 = "Crystal"
+text2 = "Biome"
+text3 = "Rating:"
+text4 = "1/5"
+
+for i in range(60):
+    print("#frame")
+    if i != 0:
+        print("clear 45 60 92")
+    drawText(text1[0], 305, 200, 255, 255, 255)
+    drawText(text1[1], 310, 200, 255, 255, 255)
+    drawText(text1[2], 315, 200, 255, 255, 255)
+    drawText(text1[3], 320, 200, 255, 255, 255)
+    drawText(text1[4], 325, 200, 255, 255, 255)
+    drawText(text1[5], 330, 200, 255, 255, 255)
+    drawText(text1[6], 335, 200, 255, 255, 255)
+
+for i in range(60):
+    print("#frame")
+    if i != 0:
+        print("clear")
+    drawText(text2[0], 310, 200, 255, 255, 255)
+    drawText(text2[1], 315, 200, 255, 255, 255)
+    drawText(text2[2], 320, 200, 255, 255, 255)
+    drawText(text2[3], 325, 200, 255, 255, 255)
+    drawText(text2[4], 330, 200, 255, 255, 255)
+
+
+for i in range(60):
+    print("#frame")
+    if i != 0:
+        print("clear")
+    drawText(text3[0], 305, 200, 255, 255, 255)
+    drawText(text3[1], 310, 200, 255, 255, 255)
+    drawText(text3[2], 315, 200, 255, 255, 255)
+    drawText(text3[3], 320, 200, 255, 255, 255)
+    drawText(text3[4], 325, 200, 255, 255, 255)
+    drawText(text3[5], 330, 200, 255, 255, 255)
+    drawText(text3[6], 335, 200, 255, 255, 255)
+
+
+for i in range(60):
+    print("#frame")
+    if i != 0:
+        print("clear")
+    drawText(text4[0], 315, 200, 255, 255, 255)
+    drawText(text4[1], 320, 200, 255, 255, 255)
+    drawText(text4[2], 325, 200, 255, 255, 255)
+
+
+for i in range(50):
+    print("#frame")
+    if i != 0:
+        print("clear")
+    drawPlayer(0)
